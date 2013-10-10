109afcff8c6975aa5595ea44b0433620db1f2fbe
bd-j <benjamin.duncan.johnson@gmail.com>
2013-10-10 14:57:39 +0200

    working extinction module

--- extinction.py
+++ extinction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import warnings, sys
 import scipy.interpolate as interp
+from numpy.random import normal 
 
 
 #### Attenuator class
@@ -26,46 +27,51 @@ class Attenuator(object):
         """This will be slow until the dust curves are vectorized"""
         spec = np.atleast_2d(inspec) #memory hog
         for i,p in enumerate(pars):
-            tau = self.dustcurve.acurve(wave, A_v = p['A_V'], R_v = p['R_V'], f_bump = p['F_BUMP'])
+            tau = self.dustcurve.acurve(wave, A_v = p['A_V'],
+                                        R_v = p['R_V'],
+                                        f_bump = p['F_BUMP'],
+                                        uv_slope = p['UV_SLOPE'])
             #print(tau.shape, spec.shape)
             spec[i,:] = spec[i,:]*np.exp(-tau)
         return spec
 
     def draw_taus(self,ntau, pars):
         pass
- 
 
 #########Extinction Law Classes
 ########
 
-class Generic_Curve(object):
+class GenericCurve(object):
     """Class for handling extinction curves with the parameterization
     of Fitzpatrick & Massa 1990 or F&M 2007"""
 
-
     def __init__(self, form = 'F90', var = False):
         self.default_pars(var = var)
+
         if form is 'FM90':
-            self._F = self.F_90
+            self._f = self.f_90
         else:
-            self._F = self.F_07                        
+            self._f = self.f_07                        
  
-    def fm_curve(self, x, c1 = None, c2 = None, c3 = None, c4 = None, c5 = None, x0 = 4.59, gamma  = 0.89):
-        return  (c1 + c2*x + c3*self.drude(x, gamma, x0) + c4 * self._F(x, c5 = c5))
+    def fm_curve(self, x, c1 = None, c2 = None, c3 = None, c4 = None, c5 = None,
+                 x0 = 4.59, gamma  = 0.89, **extras):
+        """Fitzpatrick & Massa parameterization of the UV excess curve """
+        return  (c1 + c2*x + c3*self.drude(x, gamma, x0) + c4 * self._f(x, c5 = c5))
     
     def drude(self, x, gamma, x0):
         """Drude profile for the 2175AA bump"""
         return x*x / ( (x*x-x0*x0)**2 +(x*gamma)**2 )
 
-    def F_90(self, x, **extras):
-        """UV rise in the extinction curve.  defined cubic polynomial"""
+    def f_90(self, x, **extras):
+        """UV rise in the excess curve.  FM90 (defined cubic polynomial)"""
         return (x > 5.9)*(0.5392*(x-5.9)*(x-5.9)+0.05644*(x-5.9)**3)
 
-    def F_07(self, x, c5 = 5.9, **extras):
-        """UV rise in the extinction curve, FM07 (simple quadratic with variable center)"""
+    def f_07(self, x, c5 = 5.9, **extras):
+        """UV rise in the excess curve, FM07 (quadratic with variable center)"""
         return (x > c5)*(x-c5)*(x-c5)
 
-    def powerlaw(self, x, R_v = 3.1, k = None, alpha = -1.84):
+    def powerlaw(self, x, R_v = 3.1, k = None, alpha = -1.84, **extras):
+        """Power-law shape of excess curve, for the NIR"""
         return k*(1./x)**alpha - R_v
 
     def spline(self, x, spline_x = [1.8,2.5,3.0], spline_k = [0.0,1.32, 2.02]):
@@ -75,30 +81,32 @@ class Generic_Curve(object):
         return sp(x)
         
     def acurve(self, wave, **pars):
+        """Return the extinction curve given the excess curve"""
         x = 1e4/wave
-        self.setpars(pars)
+        self.setpars(**pars)
         return self.ecurve(x, self.pardict) * self.pardict['A_v']/self.pardict['R_v'] + 1
+        
+    def setpars(self, A_v = 1, R_v = 3.1, f_bump = 1.0, uv_slope = 1):        
+        self.pardict['c3'] *= f_bump
+        self.pardict['A_v'] = A_v
+        self.pardict['R_v'] = R_v
+        self.pardict['c2'] *= uv_slope
+        self.selfupdate()
 
 
-class F99(Generic_Curve):
-    pass
-
-class LMC(Generic_Curve):
-    pass
-
-class SMC(Generic_Curve):
-    pass
-
-class FM07(Generic_Curve):
+class FM07(GenericCurve):
     """ Extinction curves from Fitzpatrick and Massa 2007"""
 
+    def __init__(self, var = False):
+        self.default_pars(var = var)
+        self._f = self.f_07                        
+
     def default_pars(self, var = False):
-        """FM99 parameterization coefficients."""
-        from numpy.random import normal 
+        """FM07 parameterization coefficients.  Set var to True to include
+        scatter in the coefficients based on the FM07 sample."""        
         p = {}
         #UV
         p['c2'] = 0.81 + var*normal(0, 0.25)
-        p['c1'] = 2.02 - 3.007 * p['c2'] +var*normal(0, 0.3)
         p['c3'] = 2.99 + var*normal(0, 1.0)
         p['c4'] = 0.32 +var*normal(0, 0.16)
         p['c5'] = 6.10 + var*normal(0, 0.6)
@@ -110,32 +118,137 @@ class FM07(Generic_Curve):
                           + var*normal(0,1,3)*np.array([0.17, 0.026, 0.008]))
         #NIR
         p['alpha'] = -1.84
-        p['R_v'] = 3.1 +var*normal(0., 0.6)
-        p['k'] = -0.83 + 0.63*p['R_v'] + var*normal(0, 0.12)
+        p['R_v'] = 3.1 + var*normal(0., 0.6)
         
         self.pardict = p
+        self.selfupdate()
 
     def selfupdate(self, var = False):
+        """Enforce empirical relationships between some of the
+        exctinction curve parameters, optionally including variance
+        in the relationships"""
+        
         self.pardict['k'] = -0.83 + 0.63*self.pardict['R_v'] + var*normal(0, 0.12)
         self.pardict['c1'] = 2.02 - 3.007 * self.pardict['c2'] +var*normal(0, 0.3)
+
+    def ecurve(self, x, pardict):
+        """Excess curve, including 3 components (UV, Optical, and NIR)"""
+        nir = self.powerlaw( x, **pardict) 
+        uv = self.fm_curve(x, **pardict) 
+        spline_x = np.array([1e4/2600, 1e4/2700] +
+                            pardict['ospline_x'].tolist() +
+                            [1.0,0.75])
+        spline_k = np.array(self.fm_curve(spline_x[0:2], **pardict).tolist() + 
+                             pardict['ospline_k'].tolist() +
+                             self.powerlaw( spline_x[-2:], **pardict).tolist())
+        print(spline_x, spline_k)
+        optical  = self.spline(x, spline_x =spline_x[::-1], spline_k = spline_k[::-1]) 
+        return nir*(x < 0.75) + uv*(x > 3.8) + optical*((x >= 0.75) & (x <= 3.8)) 
+    
+
+class LMC(FM07):
+    def __init__(self, var = False):
+        self.default_pars(var = var)
+        self._f = self.f_90                        
+
+    def default_pars(self, var = False):
+        """Gordon 2003 parameterization coefficients.  Set var to True to include
+        scatter in the coefficients based on the Gordon 03 sample."""
+        p = {}
+        #UV
+        p['c2'] = 0.998 + var*normal(0, 0.027)
+        p['c3'] = 2.719 + var*normal(0, 0.137)
+        p['c4'] = 0.40 +var*normal(0, 0.036)
+        p['x0'] = 4.579+ var*normal(0, 0.007)
+        p['gamma'] = 0.934 + var*normal(0, 0.016)
+        #Optical
+        p['ospline_x'] = 1e4/np.array([3300., 4000., 5330])
+        p['ospline_k'] = (np.array([2.05, 1.32, 0.0])
+                          + var*normal(0,1,3)*np.array([0.17, 0.026, 0.008]))
+        #NIR
+        p['alpha'] = -1.84
+        p['R_v'] = 3.1 +var*normal(0., 0.6)
         
-    def setpars(self, inpars):
-        self.pardict['c3'] *= inpars['f_bump']
-        self.pardict['A_v'] = inpars['A_v']
-        self.pardict['R_v'] = inpars['R_v']
-        
+        self.pardict = p
         self.selfupdate()
+ 
+class SMC(FM07):
+    def __init__(self, var = False):
+        self.default_pars(var = var)
+        self._f = self.f_90                        
+
+    def default_pars(self, var = False):
+        """Gordon 2003 parameterization coefficients.  Set var to True to include
+        scatter in the coefficients based on the Gordon 03 sample."""
+        p = {}
+        #UV
+        p['c2'] = 2.264 + var*normal(0, 0.040)
+        p['c3'] = 0.389 + var*normal(0, 0.110)
+        p['c4'] = 0.46 + var*normal(0, 0.079)
+        p['x0'] = 4.60 
+        p['gamma'] = 1.0
+        #Optical
+        p['ospline_x'] = 1e4/np.array([3300., 4000., 5330])
+        p['ospline_k'] = (np.array([2.05, 1.32, 0.0])
+                          + var*normal(0,1,3)*np.array([0.17, 0.026, 0.008]))
+        #NIR
+        p['alpha'] = -1.84
+        p['R_v'] = 3.1 +var*normal(0., 0.6)
+        
+        self.pardict = p
+        self.selfupdate()
+
+
+class F99(GenericCurve):
+    """Fitzpatrick 1999 R_v dependent extinction curves.  These are a one
+    parameter family, though in practice we allow the bump strength to vary."""
+    
+    def __init__(self, var = False):
+        self.default_pars(var = var)
+        self._f = self.f_90                        
+
+    def default_pars(self, var = False):
+        """FM99 parameterization coefficients, for an R_v dependent curve.  Set var to True to include
+        scatter in the coefficients based on the FM07 sample."""
+        p = {}
+        #NIR
+        p['R_v'] = 3.1 +var*normal(0., 0.6)
+        #Optical
+        p['ospline_x'] = 1e4/np.array([4100., 4670., 5470., 6000., 12200, 26500, 3e8])
+        p['ospline_x'][-1] = 0.
+
+        #UV
+        p['c3'] = 3.23 + var*normal(0, 1.0)
+        p['c4'] = 0.41 +var*normal(0, 0.16)
+        p['x0'] = 4.596 + var*normal(0, 0.025)
+        p['gamma'] = 0.99 + var*normal(0, 0.1)
+
+        
+        self.pardict = p
+        self.selfupdate()
+
+    def selfupdate(self, var = False):
+        """Enforce empirical relationships between some of the
+        exctinction curve parameters"""
+        p={}
+        p['R_v'] = self.pardict['R_v']
+        self.pardict['c2'] = -0.824 + 4.717/p['R_v'] + var*normal(0, 0.25)
+        self.pardict['c1'] = 2.02 - 3.007 * self.pardict['c2'] +var*normal(0, 0.3)
+        self.pardict['ospline_k'] = np.array([1.208 + 0.0032 * p['R_v'] - 0.00033 * p['R_v']**2,
+                                              0.701 + 0.0016 * p['R_v'],
+                                              -0.050 + 0.0016 * p['R_v'],
+                                              -0.426 + 0.0044 * p['R_v'],
+                                              (0.829/3.1 - 1) * p['R_v'],
+                                              (0.265/3.1 - 1) * p['R_v'],
+                                              0-p['R_v'] ]) 
+
         
     def ecurve(self, x, pardict):
-        nir = self.powerlaw( x, **pardict) * (x < 0.75)
+        """Excess curve, including 2 components (UV and Optical/NIR)"""
         uv = self.fm_curve(x, **pardict) * (x > 3.8)
-        spline_x = np.array([1e4/2600, 1e4/2700, pardict['ospline_x'], 1.0,0.75])
-        spline_k = [self.fm_curve(spline_x[0:2], **pardict),
-                    pardict['ospline_k'], self.powerlaw( spline_x[-2:], **pardict)]
-        optical  = self.spline(x, spline_x =spline_x, spline_k = spline_k) * ( (x >= 0.75) & (x <= 3.8)) 
-        return nir + uv + optical
-
-    
-        
-
-
+        spline_x = np.array([1e4/2600, 1e4/2700] +
+                            pardict['ospline_x'].tolist())
+        spline_k = np.array(self.fm_curve(spline_x[0:2], **pardict).tolist() + 
+                             pardict['ospline_k'].tolist())
+        optical  = self.spline(x, spline_x =spline_x[::-1], spline_k = spline_k[::-1]) * (x <= 3.8)
+        return uv + optical
