909855023f6c6f6238c000a8bc960ca6d4730182
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2023-08-03 02:56:08 -0400

    update build system; bump to v0.3.2

--- sedpy/__init__.py
+++ sedpy/__init__.py
@@ -1,3 +1,8 @@
 from . import observate
+from . import smoothing
 from . import attenuation
 from . import reference_spectra
+try:
+    from ._version import __version__, __githash__
+except(ImportError):
+    pass
